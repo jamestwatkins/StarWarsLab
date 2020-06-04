7507546ab9d3544f38fab45b4736c096fc42d347
james <jamestheodorewatkins@gmail.com>
2020-06-04 08:55:22 -0400

    First commit for Swapi lab

---
+++ StarWarsApiPractice/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StarWarsApiPractice.Models;
+
+namespace StarWarsApiPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SwapiDal SP = new SwapiDal();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //Person p = SP.GetPerson("people", 3);
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult PersonSearch()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PersonSearch(int Id)
+        {
+            Person p = SP.GetPerson("people", Id);
+            return View(p);
+        }
+
+        public IActionResult StarshipSearch()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult StarshipSearch(int Id)
+        {
+            Starship p = SP.GetStarship("starships", Id);
+            return View(p);
+        }
+
+        public IActionResult PlanetSearch()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PlanetSearch(int Id)
+        {
+            Planet p = SP.GetPlanet("planets", Id);
+            return View(p);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StarWarsApiPractice/Models/SwapiDal.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace StarWarsApiPractice.Models
+{
+    public class SwapiDal
+    {
+        public string GetAPIJson(string endpoint, int Id)
+        {
+            string url = $"https://swapi.dev/api/{endpoint}/{Id}/";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            string output = sr.ReadToEnd();
+            return output;
+        }
+
+        public Person GetPerson(string endpoint, int Id)
+        {
+            string personData = GetAPIJson(endpoint, Id);
+            Person p = JsonConvert.DeserializeObject<Person>(personData);
+
+            return p;
+
+        }
+
+        public Starship GetStarship(string endpoint, int Id)
+        {
+            string starshipData = GetAPIJson(endpoint, Id);
+            Starship p = JsonConvert.DeserializeObject<Starship>(starshipData);
+
+            return p;
+
+        }
+
+        public Planet GetPlanet(string endpoint, int Id)
+        {
+            string planetData = GetAPIJson(endpoint, Id);
+            Planet p = JsonConvert.DeserializeObject<Planet>(planetData);
+
+            return p;
+
+        }
+    }
+}
